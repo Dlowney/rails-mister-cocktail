64666143b5096605001f7851f97047f06dff3a13
Douglas Lowney <lowney.douglas@gmail.com>
2017-05-26 16:47:55 +0200

    adding root

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:show, :new, :create]
   end
-
+  root to: "cocktails#index"
   resources :doses, only: :destroy
 end
